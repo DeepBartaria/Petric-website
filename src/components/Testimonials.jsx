386541d6f55f4f0e8e87bf195684e36269c38837
DeepBartaria <deepbartaria4141@gmail.com>
2026-04-25 19:59:00 +0530

    fix image impor

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import Slider from 'react-slick';
-import Dog from '../assets/Dog.png';
+import Dog from '../assets/dog.png';
 import quote from '../assets/quote.png';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
